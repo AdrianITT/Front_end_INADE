3cda72e576dc6f56c8bcf01b5e637e26652ce74d
AdrianITT <adrian.alvarez19@tectijuana.edu.mx>
2025-06-16 15:12:02 -0700

    factura

--- src/components/ServicioCliente/Facturacionjs/DetallesFactura.js
+++ src/components/ServicioCliente/Facturacionjs/DetallesFactura.js
@@ -107,7 +107,7 @@ const DetallesFactura = () => {
       key: "precioUnitario",
       render: (valorEnMXN) => {
         // Convertimos al vuelo si es USD
-        const convertido = (valorEnMXN).toFixed(3);
+        const convertido = (valorEnMXN).toFixed(2);
         return `$${convertido} ${esUSD ? "USD" : "MXN"}`;
       },
     },
@@ -117,7 +117,7 @@ const DetallesFactura = () => {
       key: "total",
       render: (valorEnMXN) => {
         // Convertimos al vuelo si es USD
-        const convertido = (valorEnMXN).toFixed(3);
+        const convertido = (valorEnMXN).toFixed(2);
         return `$${convertido} ${esUSD ? "USD" : "MXN"}`;
       },
     },
